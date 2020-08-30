7bd3c83c9b3316e1bdd2ef37630718f8826d3cde
Nicolas Montanez <nicomontanez98@gmail.com>
2020-08-30 17:46:26 -0500

    Add index.js and change server.js for app.js

--- Backend/src/database.js
+++ Backend/src/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const URI = process.env.MONGODB_URI
   ? process.env.MONGODB_URI
-  : "mongodb://localhosy/databasetest";
+  : "mongodb://localhost/databasetest";
 
 mongoose.connect(URI, {
   useNewUrlParser: true,
