046c6e3af312734a3f68faef5898e6f2d548c359
Nicolas Montanez <nicomontanez98@gmail.com>
2020-08-30 18:59:29 -0500

    Delete app.js outside the directory and update the product.model.js

---
+++ Backend/src/models/product.model.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require("mongoose");
+
+const productSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    quantity: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    }
+},{timestamps: true});
+
+const Product = model('Product', productSchema);
+module.exports = Product;
--- app.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const app = require("./app");
-require("./database");
-const { create } = require('./controllers/product.controller');
-
-app.post('/products', create);
-
-app.listen(app.get('port'), () => console.log('App listening on port ', app.get('port')));
-
