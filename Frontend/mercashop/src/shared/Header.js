5d9f337a3555509b2e779e01e5872406405a6f7c
Nicolas Montanez <nicomontanez98@gmail.com>
2020-09-07 01:36:12 -0500

    Arreglé un problema en el header

--- Frontend/mercashop/src/shared/Header.js
+++ Frontend/mercashop/src/shared/Header.js
@@ -60,7 +60,7 @@ export default function Header() {
                 </Link>
               </li>
               <li className="nav-item active">
-                {customerInfo || providerInfo ? 
+                {customerInfo ? 
                 (
                   <div className="dropdown-menu">                
                     <Link to="/perfil">
