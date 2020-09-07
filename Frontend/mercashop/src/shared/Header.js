586cbc4fa7a136b62a462376eb6295a684dcd7d5
Andres Ruiz <johnuribe17@yahoo.com>
2020-09-07 12:50:48 -0500

    agregar botones header y carrito

--- Frontend/mercashop/src/shared/Header.js
+++ Frontend/mercashop/src/shared/Header.js
@@ -1,25 +1,142 @@
-import React from "react";
-import { useSelector } from 'react-redux'
+// import React from "react";
+// import { useSelector } from 'react-redux'
+// import logo from "./../assets/images/Merca Shop letters inline.png";
+// import { Link } from "react-router-dom";
+// import { SearchIcon } from "@primer/octicons-react";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import {
+//   faUserCircle,
+//   faMapMarker,
+//   faShoppingBasket,
+//   faShoppingCart,
+// } from "@fortawesome/free-solid-svg-icons";
+
+// export default function Header() {
+//   const customerSignIn = useSelector((state) => state.customerSignIn);
+//   const { customerInfo } = customerSignIn;
+
+//   return (
+//     <div className="App">
+//       <nav className="navbar navbar-expand-lg navbar-light bg-white border-bottom">
+//         <Link className="navbar-brand" to="/home">
+//           <img src={logo} alt="logo" width="180" height="90" />
+//         </Link>
+//         <div className="row container">
+//           <div className="col-md-4">
+//             <div className="input-group ml-5">
+//               <form className="form-inline my-2 my-lg-0">
+//                 <input
+//                   className="form-control mr-sm-2 input-lg border border-dark"
+//                   type="search"
+//                   placeholder="Que quieres?"
+//                   aria-label="Search"
+//                   title="Busca"
+//                 />
+//                 <div className="input-group-append">
+//                   <button
+//                     className="btn btn-outline-dark"
+//                     type="button"
+//                     id="button-addon2"
+//                   >
+//                     <SearchIcon size={20} />
+//                   </button>
+//                 </div>
+//               </form>
+//             </div>
+//           </div>
+//         </div>
+//         <div className="col-md-8">
+//           <div className="collapse navbar-collapse" id="navbarNav">
+//             <ul className="navbar-nav ml-auto">
+//               <li className="nav-item active">
+//                 <Link className="navbar-link" to="/">
+//                   <button
+//                     className="btn btn-success btn-circle text-center ml-4 pb-1 "
+//                     alt="dirección"
+//                     title="Dirección"
+//                   >
+//                     <FontAwesomeIcon icon={faMapMarker} /> Dirección
+//                   </button>
+//                 </Link>
+//               </li>
+//               <li className="nav-item active">
+//                 {/* {customerInfo || providerInfo ?  */}
+//                 {customerInfo ? 
+//                 (
+//                   <div className="dropdown-menu">                
+//                     <Link to="/perfil">
+
+//                     </Link>
+//                   </div>
+//                 ) : (
+//                   <Link className="navbar-link" to="/login">
+//                     <button
+//                       className="btn btn-primary text-center ml-4 pb-1"
+//                       data-toggle="modal"
+//                       data-target="#formLogin"
+//                       alt="login"
+//                       title="Login"
+//                     >
+//                       <FontAwesomeIcon icon={faUserCircle} /> Registro
+//                     </button>
+//                   </Link>
+//                 )}
+//               </li>
+//               <li className="nav-item active">
+//                 <button
+//                   className="btn btn-warning text-center ml-4 pb-1 "
+//                   alt="pedidos"
+//                   title="Pedidos"
+//                 >
+//                   <FontAwesomeIcon icon={faShoppingBasket} /> Pedidos
+//                 </button>
+//               </li>
+//               <li className="nav-item active">
+//                 <button
+//                   className="btn btn-danger   text-center ml-4 pb-1 "
+//                   alt="carrito"
+//                   title="Carrito de compras"
+//                 >
+//                   <FontAwesomeIcon icon={faShoppingCart} /> Carrito
+//                 </button>
+//               </li>
+//             </ul>
+//           </div>
+//         </div>
+//         <button
+//           className="navbar-toggler"
+//           type="button"
+//           data-toggle="collapse"
+//           data-target="#navbarNav"
+//           aria-controls="navbarNav"
+//           aria-expanded="false"
+//           aria-label="Toggle navigation"
+//         >
+//           <span className="navbar-toggler-icon"></span>
+//         </button>
+//       </nav>
+//     </div>
+//   );
+// }
+
+
+import React, { useState } from "react";
+// import { useSelectoruseDispatch } from 'react-redux' // No ha sido utilizado/
 import logo from "./../assets/images/Merca Shop letters inline.png";
 import { Link } from "react-router-dom";
 import { SearchIcon } from "@primer/octicons-react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faUserCircle,
-  faMapMarker,
-  faShoppingBasket,
-  faShoppingCart,
-} from "@fortawesome/free-solid-svg-icons";
+import Carrito from "../components/Carrito";
+import { Pedidos, Login, Location } from "../shared/Buttons";
 
 export default function Header() {
-  const customerSignIn = useSelector((state) => state.customerSignIn);
-  const { customerInfo } = customerSignIn;
+  const [search, setSearch] = useState(""); //No ha sido utilizado
+  // const customerSignIn = useSelector((state) => state.customerSignIn); // No ha sido utilizado/
 
   return (
     <div className="App">
-      <nav className="navbar navbar-expand-lg navbar-light bg-white border-bottom">
+      <nav className="navbar navbar-expand-lg navbar-light bg-white border-bottom p-1">
         <Link className="navbar-brand" to="/home">
-          <img src={logo} alt="logo" width="180" height="90" />
+          <img src={logo} alt="logo" width="150" />
         </Link>
         <div className="row container">
           <div className="col-md-4">
@@ -31,6 +148,7 @@ export default function Header() {
                   placeholder="Que quieres?"
                   aria-label="Search"
                   title="Busca"
+                  onChange={(e) => setSearch(e.target.value)}
                 />
                 <div className="input-group-append">
                   <button
@@ -44,76 +162,30 @@ export default function Header() {
               </form>
             </div>
           </div>
-        </div>
-        <div className="col-md-8">
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item active">
-                <Link className="navbar-link" to="/">
-                  <button
-                    className="btn btn-success btn-circle text-center ml-4 pb-1 "
-                    alt="dirección"
-                    title="Dirección"
-                  >
-                    <FontAwesomeIcon icon={faMapMarker} /> Dirección
-                  </button>
-                </Link>
-              </li>
-              <li className="nav-item active">
-                {/* {customerInfo || providerInfo ?  */}
-                {customerInfo ? 
-                (
-                  <div className="dropdown-menu">                
-                    <Link to="/perfil">
-
-                    </Link>
-                  </div>
-                ) : (
-                  <Link className="navbar-link" to="/login">
-                    <button
-                      className="btn btn-primary text-center ml-4 pb-1"
-                      data-toggle="modal"
-                      data-target="#formLogin"
-                      alt="login"
-                      title="Login"
-                    >
-                      <FontAwesomeIcon icon={faUserCircle} /> Registro
-                    </button>
-                  </Link>
-                )}
-              </li>
-              <li className="nav-item active">
-                <button
-                  className="btn btn-warning text-center ml-4 pb-1 "
-                  alt="pedidos"
-                  title="Pedidos"
-                >
-                  <FontAwesomeIcon icon={faShoppingBasket} /> Pedidos
-                </button>
-              </li>
-              <li className="nav-item active">
-                <button
-                  className="btn btn-danger   text-center ml-4 pb-1 "
-                  alt="carrito"
-                  title="Carrito de compras"
-                >
-                  <FontAwesomeIcon icon={faShoppingCart} /> Carrito
-                </button>
-              </li>
-            </ul>
+          <div className="col-md-8">
+            <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
+              <Link className="navbar-link" to="/">
+                <Location />
+              </Link>
+              <Link className="navbar-link" to="/login">
+                <Login />
+              </Link>
+              <Pedidos />
+              <Carrito />
+            </div>
           </div>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
         </div>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
       </nav>
     </div>
   );
