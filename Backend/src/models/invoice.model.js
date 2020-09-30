5149dbff1a698459e65eb1aaaf57dca237cfb171
Nicolas Montanez <nicomontanez98@gmail.com>
2020-09-30 09:59:51 -0500

    Se agregó el modelo de factura

---
+++ Backend/src/models/invoice.model.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const invoiceSchema = new Schema(
+  {
+    customer: {
+      type: String,
+      required: true,
+    },
+    products: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+    adress: {
+      type: String,
+    },
+    city: {
+      type: String,
+    },
+    payment: {
+      type: String,
+    },
+    itemsPrice: {
+      type: Number,
+    },
+    taxPrice: {
+      type: Number,
+    },
+    shippingPrice: {
+      type: Number,
+    },
+    totalPrice: {
+      type: Number,
+    },
+    numberInvoice: {
+      type: Number,
+    },
+    paidAt: {
+      type: Date,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    isDelivered: {
+      type: Boolean,
+      default: false,
+    },
+    deliveredAt: {
+      type: Date,
+    },
+  },
+  { timestamps: true }
+);
+
+const Invoice = model("Invoice", invoiceSchema);
+module.exports = Invoice;
