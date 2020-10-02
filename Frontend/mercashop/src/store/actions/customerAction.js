7dda1e12c9e6547b5ccbbcbbc51a27d1beaf171d
Nicolás Montañez <48807699+NicolasAMontanez98@users.noreply.github.com>
2020-10-01 20:33:21 -0500

    Update customerAction.js
    Se eliminó console.log

--- Frontend/mercashop/src/store/actions/customerAction.js
+++ Frontend/mercashop/src/store/actions/customerAction.js
@@ -129,11 +129,9 @@ const register = (
         password,
       }
     );
-    console.log("customer", data);
     dispatch({ type: CUSTOMER_REGISTER_SUCCESS, payload: data });
     localStorage.setItem("customerInfo", JSON.stringify(data));
   } catch (error) {
-    console.log("error customer", error);
     dispatch({ type: CUSTOMER_REGISTER_FAIL, payload: error.message });
   }
 };
